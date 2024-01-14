78105a4309b03b2b6420b317d20ec95ff966d4be
beniewaku <78078820+beniewaku@users.noreply.github.com>
2024-01-14 23:29:57 +0000

    using formatted strings and introducing int and input to make code quicker and shorter

---
+++ getting user input in python.py
@@ -0,0 +1,18 @@
+# getting data from our users
+# how to get someone to insert text into program using input function
+my_name = "Benie"
+your_name = input("Enter your name:")
+
+#input allows text to be written text or numbers
+
+print(f"Hello {your_name}. My name is {my_name}")
+
+# try the same thing with age
+
+age = input("Enter your age:")
+age_num = int(age)
+print(f"You have lived for {age_num * 12} months")
+
+
+age = int(input("enter your age:"))
+print(f"You have lived for {age * 12 } months.")

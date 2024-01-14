6f3fe910ba8e4ccc0ad3f1c2a62bf81e5f6b263f
beniewaku <78078820+beniewaku@users.noreply.github.com>
2024-01-14 22:27:13 +0000

    using remainders to figure out odd and even numbers and calculations

---
+++ calculating the remainder of a divison.py
@@ -0,0 +1,21 @@
+integer_division = 13 // 5
+print(integer_division)
+
+# using modulus apparatus
+remainder = 13 % 5
+print(remainder)
+
+#find out whether a number is odd or even using modulus
+# the remainder when divided by 2 is always 0 when divided by a odd number the remainder is 1
+#10 / 2
+#14 / 2
+#6 / 2
+#340 / 2
+
+# 11 / 2
+# 27 / 2
+# 3 /2
+
+x = 37 #calculate if odd or even
+remainder = x % 2
+print(remainder)

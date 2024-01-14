94e358a2fb7a2f58b852feb57a555851b259d370
beniewaku <78078820+beniewaku@users.noreply.github.com>
2024-01-14 22:20:14 +0000

    Numbers in python, basic operations, floats and integers

---
+++ Numbers in python.py
@@ -0,0 +1,16 @@
+# integers and float numbers
+# hashtags are ignored used for notes
+
+age = 35 #integer
+PI = 3.14159 #float
+
+maths_operation = 1 + 3 * 4 / 2 - 2
+print(maths_operation)
+
+#float divison example
+float_division = 12 / 3
+print(float_division)
+
+# to get rid of the integer division (the decimal) to have a whole number
+integer_division = 12 // 3
+print(integer_division)

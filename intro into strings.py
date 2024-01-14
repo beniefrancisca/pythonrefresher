a4facc81b0fc9f7f382c08e236eabcd26ccb9c5c
beniewaku <78078820+beniewaku@users.noreply.github.com>
2024-01-14 22:49:39 +0000

    how to print text using doube and single quotes and the use of multiline strings and adding strings together

---
+++ intro into strings.py
@@ -0,0 +1,16 @@
+# dealing with strings (text)
+
+my_string = "Hello, world!"
+print(my_string)
+
+#sometimes can you single quotes or double quotes
+
+another_with_quotes = 'He said \"You are amazing!\" yesterday'
+
+mutliline_strings = """
+Hello worlld 
+my name is Benie. welcome to my program.
+"""
+print(mutliline_strings)
+
+# you can add strings together

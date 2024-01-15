08fac42fb6a1e3376ca9afc7c848aaa7c91f3edb
beniewaku <78078820+beniewaku@users.noreply.github.com>
2024-01-15 13:22:05 +0000

    completing coding exercise communicating with users

---
+++ coding exercise communicating with users.py
@@ -0,0 +1,11 @@
+# First, ask the user for their name. Then, print out the greeting "Hello, NAME"
+user_name = input("Enter your name: ")
+print(f"Hello, {user_name}")
+
+# Remember the uppercase H, the comma, and the space between words!
+
+# Secondly, ask the user for their age and convert it into a number.
+# Then, print out how many months that equals to (you'll have to multiply the age by 12).
+
+user_age = int(input("Enter your age: "))
+print(user_age * 12)

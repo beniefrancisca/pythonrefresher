7eef262bb1ec8fe5de0119ae95824789e4239d0b
beniewaku <78078820+beniewaku@users.noreply.github.com>
2024-01-15 13:28:13 +0000

    basics of booleans

---
+++ Boolens and comparisons in python.py
@@ -0,0 +1,14 @@
+# true and false
+age = 20
+is_over_age = age >= 18
+is_under_age = age < 18
+is_twenty = age == 20 # equal to true
+
+# two equal signs used for comparisons
+
+# creating program that shows this
+
+my_number = 5
+user_number = int(input("Enter a number: "))
+matches = my_number == user_number
+print(f"You got it right: {matches}.")

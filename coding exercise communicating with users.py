a2b345a49bcf59d860659a4e596ed75db9185715
beniewaku <78078820+beniewaku@users.noreply.github.com>
2024-01-15 14:13:44 +0000

    adding lists

--- coding exercise communicating with users.py
+++ coding exercise communicating with users.py
@@ -1,11 +1,19 @@
 # First, ask the user for their name. Then, print out the greeting "Hello, NAME"
-user_name = input("Enter your name: ")
-print(f"Hello, {user_name}")
+# user_name = input("Enter your name: ")
+# print(f"Hello, {user_name}")
 
 # Remember the uppercase H, the comma, and the space between words!
 
 # Secondly, ask the user for their age and convert it into a number.
 # Then, print out how many months that equals to (you'll have to multiply the age by 12).
 
-user_age = int(input("Enter your age: "))
-print(user_age * 12)
+# user_age = int(input("Enter your age: "))
+# print(user_age * 12)
+
+# x = True
+# cmp = x and 18
+# print(cmp)
+
+age = int(input("Enter your age: "))
+side_job = True
+print(age > 18 and age < 65 or side_job)
---
+++ lists.py
@@ -0,0 +1,15 @@
+# learning about lists
+friends =["rolf", "bob", "anne"]
+print(friends[0]) #using indexes indexes start from 0
+
+# adding people on the list using .append()
+
+friends = ["rolf", "bob", "anne"]
+friends.append("Jen")
+print(friends)
+
+# using .remove() to take someone out of the list
+
+friends.remove("bob")
+print(friends)
+
